afcf64f05e9d2ceff2dfe9e376fae56fcdd00e3e
陈 唐 <1601865659@qq.com>
2023-01-31 18:46:49 +0800

    添加项目文件。

---
+++ Rabbit-Core/Rendering/Buffer/IndexBufferObject.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL4;
+
+namespace Rabbit_Core.Rendering.Buffer
+{
+    public class IndexBufferObject : IDisposable
+    {
+        private readonly int _id;
+        public IndexBufferObject(uint[] indices)
+        {
+            _id = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer,_id);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(uint),indices,BufferUsageHint.StaticDraw);
+        }
+
+        public void Bind()
+        {
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, _id);
+        }
+
+        public void Unbind()
+        {
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, 0);
+        }
+
+
+        private void ReleaseUnmanagedResources()
+        {
+            GL.DeleteBuffer(_id);
+        }
+
+        public void Dispose()
+        {
+            ReleaseUnmanagedResources();
+            GC.SuppressFinalize(this);
+        }
+
+        ~IndexBufferObject()
+        {
+            ReleaseUnmanagedResources();
+        }
+    }
+}
---
+++ Rabbit-Sandbox/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Rabbit_Sandbox
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using Window window = new Window(800, 600, "LearnOpenTK");
+            //Run takes a double, which is how many frames per second it should strive to reach.
+            //You can leave that out and it'll just update as fast as the hardware will allow it.
+            window.RenderFrequency = 60;
+            window.Run();
+        }
+    }
+}
